1a27ac3bc96804c969ddc53feb03e16728ff7690
liuyong <xin_nian13@163.com>
2025-11-14 17:10:28 +0800

    删除nonUniformResample函数

--- test.m
+++ test.m
@@ -7,7 +7,7 @@ fclose(fileID);
 
 mean_x = mean(x);
 periods_original = 0;
-for i = 1 : length(x) - 1;
+for i = 1 : length(x) - 1
     if ((x(i) < mean_x) && (x(i + 1) >= mean_x))
         periods_original = periods_original + 1;
     end
@@ -19,7 +19,7 @@ dns = 10;
 [y, by] = resample(x,ups,dns);
 mean_y = mean(y);
 periods_resampled = 0;
-for i = 1 : length(y) - 1;
+for i = 1 : length(y) - 1
     if ((y(i) < mean_y) && (y(i + 1) >= mean_y))
         periods_resampled = periods_resampled + 1;
     end
