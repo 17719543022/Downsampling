11f7299a7df8331cf40f538496079821134af75f
liuyong <xin_nian13@163.com>
2025-11-17 14:14:37 +0800

    Revert "Revert "用upsample/filter/downsample三个函数替换函数upfirdn成功""
    This reverts commit ce6c3ddf43f8f78470fcf426244e9466a1d7cbee.

--- test.m
+++ test.m
@@ -5,23 +5,8 @@ C = textscan(fileID, '%f');
 x = C{:};
 fclose(fileID);
 
-mean_x = mean(x);
-periods_original = 0;
-for i = 1 : length(x) - 1
-    if ((x(i) < mean_x) && (x(i + 1) >= mean_x))
-        periods_original = periods_original + 1;
-    end
-end
-
 ups = 7;
 dns = 10;
 
-[y, by] = uniformResample(x, 0, 1, [1,2], x, ups, dns);
+[y, by] = uniformResample(x, ups, dns);
 
-mean_y = mean(y);
-periods_resampled = 0;
-for i = 1 : length(y) - 1
-    if ((y(i) < mean_y) && (y(i + 1) >= mean_y))
-        periods_resampled = periods_resampled + 1;
-    end
-end
--- uniformResample.m
+++ uniformResample.m
@@ -1,45 +1,12 @@
-function  [y, h] = uniformResample(x, isDimValSet, Dim, dimIn, xTrue, p, q)
+function  [y, h] = uniformResample(x, p, q)
 
-% codegen inference
-if nargin < 7
-    q = 1;
-end
-
-% % number of numeric arguments in varargin
-% nNum = length(varargin);
 N = 10;
 bta = 5;
-
-% % parse N and beta
-% if ~isempty(varargin)
-%     if ((nNum >= 2 && isDimValSet)|| (nNum >= 1 && ~isDimValSet))&& ~isempty(varargin{1})
-%         N = varargin{1};
-%     end
-%     if ((nNum >= 3 && isDimValSet)|| (nNum >= 2 && ~isDimValSet))&& ~isempty(varargin{2})
-%         bta = varargin{2};
-%     end
-% end
-
-validateattributes(x, {'numeric'},{},'resample','X',1);
-validateResampleRatio(p, q);
 
 [p, q] = rat( p/q, 1e-12 );  %--- reduce to lowest terms
 % (usually exact, sometimes not; loses at most 1 second every 10^12 seconds)
 p = p(1);
 q = q(1);
-
-if (p == 1) && (q == 1)
-    yTmp = x;
-    if (numel(x) == numel(xTrue))
-        y = ipermute(yTmp,dimIn);
-        y = reshape(y,size(xTrue));
-    else
-        y = reshape(yTmp,size(x));
-        y = ipermute(y,dimIn);
-    end
-    h = 1;
-    return
-end
 
 pqmax = max(p,q);
 if length(N)>1      % use input filter
@@ -84,235 +51,28 @@ h = [h zeros(1,nZeroEnd)];
 Ly = ceil(Lx*p/q);  % output length
 % Ly = floor((Lx-1)*p/q+1);  <-- alternately, to prevent "running-off" the
 %                                data (extrapolation)
-szx = size(x);
-sy = szx;
-if isrow(x)
-    sy(2) = Ly;
-else
-    sy(1) = Ly;
-end
-
-% numel in the 3rd dimension, when nD array is collapsed to a 3D-array
-n2D = 1;
-if numel(szx) >= 3
-    n2D = prod(szx(3:length(szx)),'all');
-end
-
-% syTrue is the desired output size for uniform resample
-sxTrue = size(xTrue);
-syTrue = sxTrue;
-
-if isvector(xTrue)
-    if isrow(xTrue)
-        syTrue(2) = Ly;
-    else
-        syTrue(1) = Ly;
-    end
-else
-    syTrue(Dim) = Ly;
-end
 
 % ----  HERE'S THE CALL TO UPFIRDN  ----------------------------
-if isreal(x)
-    y = coder.nullcopy(zeros(syTrue));
-else
-    y = coder.nullcopy(complex(ones(syTrue),ones(syTrue)));
-end
+y = coder.nullcopy(zeros(Ly,1));
+
+xTrue = [x; zeros(size(h,2),1)];
 
 if isvector(x)
-    yVec = upfirdn(x,h,p,q);
+    yUp = upsample(xTrue, p);
+    yFir = filter(h, 1, yUp);
+    yDn = downsample(yFir, q);
+    yVec = [yDn;zeros(size(h,2),1)];
     indV = delay+(1:Ly);
     
     if ~isrow(x)
         indV = indV.';
         yV = yVec(indV);
         y = yV;
-    else
-        yV = yVec(indV);
-        yV1 = yV.';
-        if (numel(x) == numel(xTrue))
-            y = reshape(yV1,syTrue);       % % uniform resample
-        else
-            y = reshape(yV1,sy);           % % non-uniform resample
-        end
-    end
-elseif ~isvector(x) && (numel(szx)==2)
-    ymat = upfirdn(x,h,p,q);
-    indM = delay+(1:Ly);
-    indM = indM.';
-    yM=ymat(indM,:);
-    
-    if (numel(ymat) == prod(syTrue))
-        yRshp = ipermute(yM,dimIn);
-        y = reshape(yRshp,syTrue);
-    else
-        y = ipermute(yM,dimIn);       % % non-uniform resample
-    end
-end
-
-% input is an nD-array
-if (numel(szx)> 2)
-    syTmp = [sy(1:2) n2D];
-    if isreal(x)
-        yTmp = coder.nullcopy(zeros(syTmp));
-    else
-        yTmp = coder.nullcopy(complex(ones(syTmp),ones(syTmp)));
-    end
-    
-    for ii = 1:n2D
-        xTmp = x(:,:,ii);
-        
-        if (szx(1) == 1)
-            % if xTmp is a row vector and user sets Dimension to the singleton dimension
-            if isreal(x)
-                yTmp1 = coder.nullcopy(zeros(sy(1),sy(2)));
-            else
-                yTmp1 = coder.nullcopy(complex(ones(sy(1),sy(2)),ones(sy(1),sy(2))));
-            end
-            
-            for jj = 1: szx(2)
-                xTmp1 = x(1,jj,ii);
-                yTmp2 = upfirdn(xTmp1,h,p,q).';
-                indTmp = (1:Ly)+delay;
-                indTmp = indTmp.';
-                y1 = yTmp2(indTmp,:);
-                yTmp1(1:Ly,jj) = y1;
-            end
-            yTmp(:,:,ii) = yTmp1;
-            
-        else
-            yTmp1 = upfirdn(xTmp,h,p,q);
-            ind = (1:Ly)+delay;
-            ind = ind.';
-            y1 = yTmp1(ind,:);
-            yTmp(:,:,ii) = y1;
-        end
-    end
-    
-    if (numel(syTmp) ~= numel(sy))      % nonuniform resample
-        yRshp = reshape(yTmp,sy);       % converting 3D output to nD
-        y1 = ipermute(yRshp,dimIn);      % returns to the original orientation
-        y = reshape(y1,syTrue);          % ensures the correct output size
-    else
-        y1 = ipermute(yTmp,dimIn);
-        y = reshape(y1,syTrue);
     end
 end
 
 h = h(nZeroBegin+1:end-nZeroEnd);  % get rid of leading and trailing zeros
 % in case filter is output
-end
-%-------------------------------------------------------------------------
-function validateResampleRatio(p, q)
-validateattributes(p, {'numeric'},{'integer','positive','finite', ...
-    'scalar'},'resample','P');
-validateattributes(q, {'numeric'},{'integer','positive','finite', ...
-    'scalar'},'resample','Q');
-end
-%-------------------------------------------------------------------------
-function [method] = getInterpMethod(varargin)
-
-method = '';
-supportedMethods = {'linear','pchip','spline'};
-for i=1:numel(varargin)
-    if ischar(varargin{i})|| isStringScalar(varargin{i})
-        if (strncmpi(varargin{i},'Dimension',3))
-            break;
-        else
-            method = validatestring(varargin{i},supportedMethods,'resample','METHOD');
-        end
-    end
-end
-end
-
-%-------------------------------------------------------------------------
-function [xOut, dimPerm] = orientToNdim (x, nDim)
-sX = size(x);
-
-% error if Dimenion is larger than input size
-coder.internal.assert(~(~isscalar(x) && (numel(sX)< nDim)),...
-    'signal:resample:DimensionMismatch');
-
-trueDim = 1:numel(sX);
-dimPerm = coder.nullcopy(trueDim);
-
-if isvector(x)
-    xOut = x;
-    dimPerm = trueDim;
-elseif (numel(sX) >= 2)
-    % rotate the input according to Dimension
-    if (nDim ~= 1)
-        [~,inds] = sort(sX,'descend');
-        i1 = (inds ~= nDim);
-        dimPerm(1) = nDim;
-        dimPerm(2:end) = inds(i1);
-        xOut = permute(x, dimPerm);
-    else
-        xOut = x;
-        dimPerm = trueDim;
-    end
-end
-end
-%-------------------------------------------------------------------------
-function [dim,m,isDimValSet] = dimParser(varargin)
-
-% By default, set nDim to the first non-singleton dimension
-sx = size(varargin{1});
-dim = 1;
-
-if numel(sx) > 1 && ~isscalar(varargin{1})
-    nonSingDim = find(sx>1);
-    dim = nonSingDim(1);
-end
-
-% flags to set
-dimSet = false;
-isDimValSet = false;
-possibleStrings = {'linear','pchip','spline','Dimension'};
-
-coder.unroll;
-for argIndex = 1:length(varargin)
-    inputVal = varargin{argIndex};
-    if ischar(inputVal) || isStringScalar(inputVal)
-        str = validatestring(inputVal,possibleStrings,'resample');
-        switch str
-            case 'Dimension'
-                coder.internal.assert(~dimSet, ...
-                    'signal:resample:MultipleNameEntered');
-                dimSet = true;
-        end
-    elseif isnumeric(inputVal)
-        if (dimSet) && (argIndex == length(varargin))
-            validateattributes(inputVal,{'numeric'},...
-                {'scalar','finite','real','positive'},'resample','Dim');
-            dim = double(inputVal);
-            isDimValSet = true;
-        end
-    end
-end
-
-coder.internal.assert(~(dimSet && ~isDimValSet),...
-    'signal:resample:NameValueExpected');
-
-n = coder.const(countNumericInputs(varargin{:}));
-
-% the number of numeric arguments in varargin after removing Dimension
-if dimSet && isDimValSet
-    m = n-1;                            % user specified Dim
-else
-    m = n;
-end
-
-end
-
-function n = countNumericInputs(varargin)
-n = 0;
-coder.unroll;
-for k = 1:nargin
-    if isnumeric(varargin{k}) || isdatetime(varargin{k})
-        n = n + 1;
-    end
-end
 end
 
 function nZeroEnd = computeZeroPadLength(Lx,p,q,lenH,delay)
